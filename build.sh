c58f943cded48f3f1a520915900bf783ea48d792
judehahh <liujh20121601@live.com>
2023-12-15 21:46:28 +0800

    Import commmon, hal, driver and freertos compatibility layers.

--- build.sh
+++ build.sh
@@ -30,6 +30,9 @@ popd
 if [ ! -e $BUILD_FREERTOS_PATH/threadx ]; then
     mkdir -p $BUILD_FREERTOS_PATH/threadx
 fi
+if [ ! -e $BUILD_FREERTOS_PATH/kernel ]; then
+    mkdir -p $BUILD_FREERTOS_PATH/kernel
+fi
 pushd $BUILD_FREERTOS_PATH/threadx
 cmake -G Ninja \
     -DTHREADX_ARCH=$THREADX_ARCH \
@@ -37,6 +40,48 @@ cmake -G Ninja \
     -DTOP_DIR=$TOP_DIR \
     -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE_PATH \
     $TOP_DIR/threadx
+cmake --build . --target install -- -v
+popd
+
+if [ ! -e $BUILD_FREERTOS_PATH/common ]; then
+    mkdir -p $BUILD_FREERTOS_PATH/common
+fi
+pushd $BUILD_FREERTOS_PATH/common
+cmake -G Ninja -DCHIP=$RUN_CHIP  \
+    -DRUN_ARCH=$RUN_ARCH \
+    -DTOP_DIR=$TOP_DIR \
+    -DBUILD_ENV_PATH=$BUILD_ENV_PATH \
+    -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE_PATH \
+    $TOP_DIR/common
+cmake --build . --target install -- -v
+popd
+
+if [ ! -e $BUILD_FREERTOS_PATH/hal ]; then
+    mkdir -p $BUILD_FREERTOS_PATH/hal
+fi
+pushd $BUILD_FREERTOS_PATH/hal/
+cmake -G Ninja -DCHIP=$RUN_CHIP  \
+    -DRUN_ARCH=$RUN_ARCH \
+    -DTOP_DIR=$TOP_DIR \
+    -DRUN_TYPE=$RUN_TYPE \
+    -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE_PATH \
+    -DBOARD_FPGA=n \
+    $TOP_DIR/hal
+cmake --build . --target install -- -v
+popd
+
+if [ ! -e $BUILD_FREERTOS_PATH/driver ]; then
+    mkdir -p $BUILD_FREERTOS_PATH/driver
+fi
+pushd $BUILD_FREERTOS_PATH/driver
+cmake -G Ninja -DCHIP=$RUN_CHIP  \
+    -DRUN_ARCH=$RUN_ARCH \
+    -DTOP_DIR=$TOP_DIR \
+    -DRUN_TYPE=$RUN_TYPE \
+    -DBUILD_ENV_PATH=$BUILD_ENV_PATH \
+    -DBOARD_FPGA=n \
+    -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE_PATH \
+    $TOP_DIR/driver
 cmake --build . --target install -- -v
 popd
 
